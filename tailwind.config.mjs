564c886fe2deab44f8503a0ce1f96b2e33574df1
dany <dacr63@gmail.com>
2024-03-04 18:38:28 +0100

    feat: initial cfg

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          bg: 'rgb(var(--primary-bg) / <alpha-value>)',
+          'back-bg': 'rgb(var(--primary-back-bg) / <alpha-value>)',
+          text: 'rgb(var(--primary-text) / <alpha-value>)',
+          accent: 'rgb(var(--primary-accent) / <alpha-value>)',
+        },
+        secondary: {
+          bg: 'rgb(var(--secondary-bg) / <alpha-value>)',
+        },
+        label: {
+          bg: 'rgb(var(--label-bg) / <alpha-value>)',
+        },
+      },
+      fontFamily: {
+        sans: ['Jost Variable', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {},
+      animation: {},
+    },
+  },
+  plugins: [],
+};
