420043bd2f6ef25f5ab22211ba42e7bae49576a2
dany <dacr63@gmail.com>
2024-03-18 22:53:23 +0100

    feat configure prettier

--- src/components/home/components/AccordionWrapper.tsx
+++ src/components/home/components/AccordionWrapper.tsx
@@ -1,36 +1,29 @@
-import { type FC } from "react"
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "./Accordion"
+import { type FC } from 'react'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './Accordion'
 
 interface IAccordionWrapperProps {
-  items: {
-    itemNo: number
-    triggerText: string
-    contentText: string
-  }[]
+	items: {
+		itemNo: number
+		triggerText: string
+		contentText: string
+	}[]
 }
 
 const AccordionWrapper: FC<IAccordionWrapperProps> = ({ items }) => {
-  return (
-    <Accordion type='single' collapsible>
-      {items.map((item) => (
-        <AccordionItem
-          className='data-[state=open]:bg-primary-bg-component px-4 data-[state=open]:rounded-xl transition-all'
-          key={item.itemNo}
-          value={`item-${item.itemNo}`}
-        >
-          <AccordionTrigger className='text-start'>
-            {item.triggerText}
-          </AccordionTrigger>
-          <AccordionContent>{item.contentText}</AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
-  )
+	return (
+		<Accordion type="single" collapsible>
+			{items.map((item) => (
+				<AccordionItem
+					className="px-4 transition-all data-[state=open]:rounded-xl data-[state=open]:bg-primary-bg-component"
+					key={item.itemNo}
+					value={`item-${item.itemNo}`}
+				>
+					<AccordionTrigger className="text-start">{item.triggerText}</AccordionTrigger>
+					<AccordionContent>{item.contentText}</AccordionContent>
+				</AccordionItem>
+			))}
+		</Accordion>
+	)
 }
 
 export default AccordionWrapper
