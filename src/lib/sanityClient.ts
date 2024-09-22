99efaad3085db143d75e2340121a2fa13f5c72e3
Jesus-Navas <jesusnavas@outlook.com>
2024-09-23 01:01:44 +0200

    cdn true

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -4,7 +4,7 @@ export const client = createClient({
   projectId: import.meta.env.SANITY_PROJECT_ID as string,
   dataset: import.meta.env.SANITY_DATASET as string,
   token: import.meta.env.SANITY_TOKEN as string | undefined,
-  useCdn: false,
+  useCdn: true,
   apiVersion: '2023-01-01', 
 });
 
